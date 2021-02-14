d36cf1e857cd879fc6062b77c6f48d15c6039ed0
Vedparkash5 <vedparkashsingh690@gmail.com>
2021-02-14 01:01:14 -0800

    bob

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
 // Script.js
+myStorage = window.localStorage;
+window.addEventListener('DOMContentLoaded', bob);  
+ 
+async function bob() {
+  if (myStorage === null) {
+    const response = await fetch (' https://fakestoreapi.com/products');
+    const jsonw = await response.json();
+    bot = JSON.stringify(jsonw);
+    myStorage.setItem('array', bot);
+  }
+}; 
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-});
+arr =  myStorage.getItem('array') ;
+bob = JSON.parse(arr);
+for (const a in bob) {
+  var u = document.createElement('product-item');
+  u.shadowRoot.querySelector('img').src =this.image;
+  
+}
